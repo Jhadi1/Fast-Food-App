8b70a5e8fde237c17e99858f16157fad343dc776
Jamilla <jayhad62@gmail.com>
2020-04-06 16:35:21 -0700

    Get order save in array not clearing out

--- Week7.js
+++ Week7.js
@@ -1,6 +1,22 @@
-function fn1()
-{
+var order = [];
+function fn1() {
     var str= document.getElementById("customerOrder").value;
-    alert("You have selected "+str);
+    alert("You have selected "+ str);
+    
     console.log(str);
-} 
+   order = str.split(',');
+console.log(order);
+};
+
+
+
+
+let menu = {
+    'fry': 10,
+    'shake': 10,
+    'burger': 10,
+    'salad': 10,
+    'drink': 10,
+    'cookie': 10
+
+}
